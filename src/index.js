7396c4cb9ee71085c8021133b4aa3a0cbda1f425
Viswa Kiran K <viswakirank@cosines.in>
2024-01-04 19:02:14 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import './index.css'
+import App from "./App"
+
+
+const Index =() =>{
+    return(
+        <>
+        <App></App>
+        </>
+    )
+}
+
+ReactDOM.render(
+<Index/>
+, document.getElementById("root"))
