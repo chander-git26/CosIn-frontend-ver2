2ec7b54bbce0ecfca4be78dce0d46f23a77aae92
Viswa Kiran K <viswakirank@cosines.in>
2024-01-05 15:32:35 +0530

    sa

--- src/features/Home/Components/Login.jsx
+++ src/features/Home/Components/Login.jsx
@@ -10,8 +10,8 @@ const Login = ({loginToggle}) => {
     <div className='absolute top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 h-full w-full '>
         <div className='h-full w-full bg-gray-500 opacity-40 relative' onClick={()=>loginToggle(prev=>!prev)}></div>
         <div className='absolute top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2'>
-      {/* <AuthLogin loginToggle={loginToggle}/> */}
-      <Userinfo/>
+      <AuthLogin loginToggle={loginToggle}/>
+      {/* <Userinfo/> */}
         </div>
     </div>
   )
