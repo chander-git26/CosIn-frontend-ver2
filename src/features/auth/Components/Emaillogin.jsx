dfabbb0806172b52a3c8a24033ca0180c2a4effc
Viswa Kiran K <viswakirank@cosines.in>
2024-01-08 10:34:08 +0530

    changes in or login btn

--- src/features/auth/Components/Emaillogin.jsx
+++ src/features/auth/Components/Emaillogin.jsx
@@ -36,13 +36,13 @@ const Emaillogin = ({ setLogintype,setOtp }) => {
                     <div className="flex flex-col my-5 relative  border-b-gray-400">
                         <Field id="email" name="email" placeholder="Enter your email" type='text' className="focus:outline-none focus:border-b-2 border-b-2 focus:border-accent-hover text-sm py-2 px-2  flex-1  placeholder:text-xs" />
                         {formik.touched.email && formik.errors.email ? (
-                            <div className='text-xs mt-2 text-accent-hover' ><i className="bi bi-exclamation-circle"></i> {formik.errors.email}</div>
+                            <div className='text-xs mt-2 text-accent-hover absolute -bottom-6 left-0' ><i className="bi bi-exclamation-circle"></i> {formik.errors.email}</div>
                         ) : null}
 
                     </div>
 
                     <button type="submit" className='bg-accent font-medium  p-3 px-4  hover:bg-accent-hover   w-auto rounded-md hover:text-white transition-all	 duration-200   text-sm'>Get OTP</button>
-                    <button className='text-sm text-center py-2 ' onClick={() => { setLogintype('mobile') }}> <span className='text-gray-500  hover:text-accent-hover  font-semibold text-xs'> {"(or)"} Login with Mobile OTP</span></button>
+                    <button className='text-sm text-center py-2 '> <span  onClick={() => { setLogintype('mobile') }} className='text-gray-500  hover:text-accent-hover cursor-pointer font-semibold text-xs'> {"(or)"} Login with Mobile OTP</span></button>
 
                 </Form>)}
         </Formik>
--- src/features/auth/Components/Mobilelogin.jsx
+++ src/features/auth/Components/Mobilelogin.jsx
@@ -33,13 +33,13 @@ function Mobilelogin({setLogintype, setOtp}) {
                     {/* <label htmlFor="mobile" className={inpLableSty}>PHONE</label> */}
                     <Field id="mobile" name="mobile" placeholder="Enter your phone number" type='text' className="focus:outline-none focus:border-b-2 border-b-2 focus:border-accent-hover text-sm py-2 px-2  flex-1  placeholder:text-xs" />
                     {formik.touched.mobile && formik.errors.mobile ? (
-                      <div  className='text-xs text-accent-hover mt-2' ><i className="bi bi-exclamation-circle"></i> {formik.errors.mobile}</div>
+                      <div  className='text-xs mt-2 text-accent-hover absolute -bottom-6 left-0' ><i className="bi bi-exclamation-circle"></i> {formik.errors.mobile}</div>
                     ) : null}
                  
                   </div>
 
                   <button type="submit" className='bg-accent font-medium  p-3 px-4 hover:bg-accent-hover   w-auto rounded-md hover:text-white transition-all	 duration-200   text-sm'>Get OTP</button>
-                  <button className='text-sm text-center py-2 ' onClick={()=>{setLogintype('email')}}> <span className='text-gray-500 font-semibold text-xs  hover:text-accent-hover '> {"(or)"} Login with E-Mail</span></button>
+                  <div className='text-sm text-center py-2 '> <span  onClick={()=>{setLogintype('email')}} className='text-gray-500 font-semibold text-xs cursor-pointer hover:text-accent-hover '> {"(or)"} Login with E-Mail</span></div>
 
                 </Form>)}
             </Formik>
