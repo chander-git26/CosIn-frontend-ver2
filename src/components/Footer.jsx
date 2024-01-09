ced5b8990a9adebf1b05b401bd8b0f7639a51eed
pavan preetham <preethamnayakanti@gmail.com>
2024-01-09 18:37:47 +0530

    added footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className=' bg-gray-100  py-2'>
+      <div className=' container flex flex-col '>
+        <div  className='  flex flex-row justify-center p-5  gap-5'>
+            <div className='  flex flex-col p-5  gap-2'>
+                <h1 className='text-gray-700 pb-3  font-bold text-sm'>Health Insurance Plans</h1>
+                <p className='font-medium text-gray-500  text-sm'>Individual Health Insurance</p>
+                <p className='font-medium text-gray-500  text-sm'>Family Health Insurance</p>
+                <p className='font-medium text-gray-500  text-sm'>Medicare and Medicaid</p>
+               
+                
+            </div>
+            
+            <div className='  flex flex-col p-5  gap-2'>
+                <h1 className='text-gray-700 pb-3  font-bold text-sm'>Life Insurance Plans</h1>
+                <p className='font-medium text-gray-500  text-sm'>Term Life Insurance</p>
+                <p className='font-medium text-gray-500  text-sm'>Whole Life Insurance</p>
+                
+            </div>
+
+            <div className='  flex flex-col p-5  gap-2'>
+                <h1  className='text-gray-700 pb-3  font-bold text-sm'>Property Insurance</h1>
+                <p className='font-medium text-gray-500  text-sm'>Homeowners Insurance</p>
+                <p className='font-medium text-gray-500  text-sm'>Renters Insurance</p>
+                <p className='font-medium text-gray-500  text-sm'>Flood Insurance</p>
+                
+            </div>
+
+            <div className='  flex flex-col p-5  gap-2'>
+                <h1 className='text-gray-700 pb-3  font-bold text-sm'>Auto  Insurance </h1>
+                <p className='font-medium text-gray-500  text-sm'>Liability Insurance</p>
+                <p className='font-medium text-gray-500  text-sm'>Collision Insurance</p>
+                <p className='font-medium text-gray-500  text-sm'>Comprehensive Insurance</p>
+                
+            </div>
+            </div>
+            
+            <div className='  flex flex-row justify-center p-5   gap-5'>
+            <div className='  flex     flex-col p-5  gap-2'>
+                <h1  className='text-gray-700 pb-3  font-bold text-sm'>Customer Service</h1>
+                <p className='font-medium text-gray-500  text-sm'>Pay Premium Online</p>
+                <p className='font-medium text-gray-500  text-sm'>Claims Centers</p>
+                <p className='font-medium text-gray-500  text-sm'>Track Applications</p>
+                <p className='font-medium text-gray-500  text-sm'>Download Premium Receipts</p>
+            </div>
+            
+            
+            <div className='  flex flex-col p-5  gap-2'>
+                <h1  className='text-gray-700 pb-3  font-bold text-sm'>About CosInsurance</h1>
+                <p className='font-medium text-gray-500 text-sm'>Why Cos Insurance</p>
+                <p className='font-medium text-gray-500  text-sm'>Know Our Story</p>
+                <p className='font-medium text-gray-500  text-sm'>Awards & Achievement</p>
+                <p className='font-medium text-gray-500  text-sm'>Careers</p>
+            </div>
+
+            <div className='  flex flex-col p-5  gap-2'>
+                <h1  className='text-gray-700 pb-3  font-bold text-sm'>Tools and Calculators</h1>
+                <p className='font-medium text-gray-500  text-sm'>Term Insurance Premium Calculator</p>
+                <p className='font-medium text-gray-500  text-sm'>Tax Calculator</p>
+                <p className='font-medium text-gray-500  text-sm'>Saving Plan Calculator</p>
+                <p className='font-medium text-gray-500  text-sm'> FD Calculator</p>
+                <p className='font-medium text-gray-500  text-sm'>CIBIL Credit Score</p>
+            </div>
+            
+
+
+
+        </div>
+        <div className='p-5 flex justify-center text-center gap-5'>
+            <i class="bi bi-facebook"></i>
+            <i class="bi bi-instagram"></i>
+            <i class="bi bi-linkedin"></i>
+            <i class="bi bi-twitter-x"></i>
+            <i class="bi bi-youtube"></i>
+        </div>
+        
+
+        </div>
+  </div>
+    )
+  }
+
+export default Footer
