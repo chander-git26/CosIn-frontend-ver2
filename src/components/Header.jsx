f8a7aa7afc7ee3694eee8a5608c06baadb58a748
Viswa Kiran K <viswakirank@cosines.in>
2024-01-09 19:31:33 +0530

    menu changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,14 +22,14 @@ const Header = ({ setDisplayLogin, displayLogin }) => {
     <>
       <div className={`fixed w-full bg-white  ${scroll?"shadow-lg":""}`}>
 
-        <div className='container  mx-auto flex justify-between my-6  inset-x-0  items-center '>
+        <div className='container  mx-auto flex justify-between mt-6  inset-x-0  items-center '>
           <div className='flex items-center gap-3'>
             <img src={logo} className='w-12' alt="Cos insurance logo" />
             <h1 className='font-bold text-2xl py-8 text-gray-600'>COS Insurance</h1>
           </div>
-          <div className='flex items-stretch'>
+          <div className='flex  items-stretch'>
             <Menu/>
-            <button onClick={() => setDisplayLogin(prev => !prev)} className='bg-accent font-medium  p-3 px-4 hover:bg-accent-hover  hover:shadow-2xl  hover:shadow-accent rounded-md hover:text-white transition-all	 duration-200   text-sm'>Login/Register</button>
+            <button  onClick={() => setDisplayLogin(prev => !prev)} className='bg-accent font-medium self-center  p-3 px-4 hover:bg-accent-hover  hover:shadow-2xl  hover:shadow-accent rounded-md hover:text-white transition-all	 duration-200   text-sm'>Login/Register</button>
           </div>
         </div>
           {displayLogin ? <Login setDisplayLogin={setDisplayLogin} /> : null}
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 const Menu = () => {
     return (
-        <div className='flex items-center  px-10 gap-8'>
-            <div className='py-5 border-b-4 border-white cursor-pointer hover:border-b-accent-hover'>Insurance Products <i className="bi bi-chevron-down text-accent-hover" style={{fontSize: "70px"}} ></i></div>
-            <div className='py-5 border-b-4 border-white cursor-pointer hover:border-b-accent-hover'>Renew Your Policy <i className="bi bi-chevron-down"></i></div>
-            <div className='py-5 border-b-4 border-white cursor-pointer hover:border-b-accent-hover'>Claim <i className="bi bi-chevron-down"></i></div>
-            <div className='py-5 border-b-4 border-white cursor-pointer hover:border-b-accent-hover'>Support <i className="bi bi-chevron-down"></i></div>
+        <div className='flex items-stretch  px-10 gap-8'>
+            <div onMo className='menuitem py-5 border-b-4 border-white cursor-pointer hover:border-b-accent-hover flex items-center relative gap-2'><h1>Insurance Products</h1> <i className=" icon bi bi-chevron-down text-accent-hover" style={{ fontSize: "18px" }}></i>
+
+              
+
+            </div>
+          
+            <div className='menuitem py-5 border-b-4 border-white cursor-pointer hover:border-b-accent-hover flex items-center gap-2'>Renew Your Policy <i className="bi bi-chevron-down text-accent-hover" style={{ fontSize: "18px" }} ></i></div>
+            <div className='menuitem py-5 border-b-4 border-white cursor-pointer hover:border-b-accent-hover flex items-center gap-2'>Claim <i className="bi bi-chevron-down text-accent-hover" style={{ fontSize: "18px" }} ></i></div>
+            <div className='menuitem py-5 border-b-4 border-white cursor-pointer hover:border-b-accent-hover flex items-center gap-2'>Support <i className="bi bi-chevron-down text-accent-hover" style={{ fontSize: "18px" }} ></i></div>
         </div>
     )
 }
